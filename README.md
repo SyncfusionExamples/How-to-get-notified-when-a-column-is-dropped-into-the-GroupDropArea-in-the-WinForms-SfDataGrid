e2949050933100e1301b098f991530fe7004afaf
Sweatha Bharathi <sweatha.bharathi@syncfusion.com>
2025-05-06 16:56:08 +0530

    To notified when a user drops a column into the group drop area

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# How-can-I-get-notified-when-a-user-drops-a-column-into-the-group-drop-area
-This demo shows how to get notified when a user drops a column into the group drop area
+# How can I get notified when a user drops a column into the group drop area
+
+In a WinForms [DataGrid](https://www.syncfusion.com/winforms-ui-controls/datagrid) to get notified when adds or removes a column in the grouping box area of the SfDataGrid control, you can use the CollectionChanged event of the [GroupColumnDescriptions](https://help.syncfusion.com/cr/windowsforms/Syncfusion.WinForms.DataGrid.SfDataGrid.html#Syncfusion_WinForms_DataGrid_SfDataGrid_GroupColumnDescriptions) property.
+
+This event allows you to detect when columns are added or removed from the drop area, as well as identify the action performed.
+
+ 
+ ```csharp
+  private void GroupColumnDescriptions_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+  {
+      if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+      {
+          // Get the dropped item to GroupDropArea
+          var addedItem = e.NewItems[0];
+      }
+
+      if(e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+      {
+          // Get the removed item from GroupDropArea
+          var removedItem = e.OldItems[0];
+      }
+  }
+
+ ```
+Take a moment to peruse the [Winforms - DataGrid Grouping UG Documentation](https://help.syncfusion.com/windowsforms/datagrid/grouping), to learn more about DataGrid Grouping with examples.
---
+++ WinformsDataGrid/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Syncfusion.WinForms.DataGrid;
+
+namespace WinformsDataGrid
+{
+    public partial class Form1 : Form
+    {
+        OrderInfoCollection orderInfoCollection;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            sfDataGrid1.Columns.Add(new GridTextColumn() { MappingName = "OrderID", HeaderText = "Order ID" });
+            sfDataGrid1.Columns.Add(new GridTextColumn() { MappingName = "CustomerID", HeaderText = "Customer ID" });
+            sfDataGrid1.Columns.Add(new GridTextColumn() { MappingName = "CustomerName", Width = 150, HeaderText = "Customer Name" });
+            sfDataGrid1.Columns.Add(new GridTextColumn() { MappingName = "Country", HeaderText = "Country" });
+            sfDataGrid1.Columns.Add(new GridTextColumn() { MappingName = "ShipCity", HeaderText = "Ship City" });
+            orderInfoCollection = new OrderInfoCollection();
+            sfDataGrid1.DataSource = orderInfoCollection.Orders;
+            sfDataGrid1.ShowGroupDropArea = true;
+            sfDataGrid1.GroupColumnDescriptions.CollectionChanged += GroupColumnDescriptions_CollectionChanged;
+        }
+
+        private void GroupColumnDescriptions_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+            {
+                // Get the dropped item to GroupDropArea
+                var addedItem = e.NewItems[0];
+            }
+
+            if(e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+            {
+                // Get the removed item from GroupDropArea
+                var removedItem = e.OldItems[0];
+            }
+        }
+    }
+}
